8c4cf58fe39898727f2d227ff68f202e04a8d467
Angelo Yanve <angelo24yanve@gmail.com>
2026-01-21 09:58:52 +0100

    init: init jakatarEE app for Rest API

---
+++ src/main/java/com/episen/tparchiweb/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.episen.tparchiweb;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
